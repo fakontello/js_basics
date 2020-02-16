a40d1c67abc5515b727e99d7f268c42b52eba127
fakontello <fakontello@gmail.com>
2020-02-16 18:42:20 +0300

    Выполнил задания к третьему уроку

---
+++ Lesson_03/Lesson_03_ex4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+let a = products.forEach(function(item){
+    console.log(item.price = item.price * 0.85)
+});
